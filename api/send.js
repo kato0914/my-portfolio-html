ae7a737f2397a7626ad6f8183f661d4679a3c996
Kato Junichi <katoj62@gmail.com>
2024-07-30 15:06:29 +0900

    database接続

--- api/send.js
+++ api/send.js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
         // リクエストの処理
         const { name, email, message } = req.body;
         // ここでメール送信やデータベースへの保存処理を行う
-        res.status(200).json({ message: 'お問合せが送信されました。' });
+        res.status(200).json({ message: 'お問合せが送信されました！' });
     } else {
         res.setHeader('Allow', ['POST']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
